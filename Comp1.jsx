602fa0b975899e29426f7ce35bd7d8ad21224e2e
ajaykumar <ajayajaykumar89424@gmail.com>
2025-12-25 20:57:31 +0530

    second

--- Comp1.jsx
+++ Comp1.jsx
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 
 const Comp1 = () => {
   const [theme , setTheme] =useState('light');
-  const toggleTheme = () =>{
+  const toggleThemeeeee = () =>{
     setTheme((beforetheme => (beforetheme==="light" ? "dark" : "light")));
   }
     
-   
+   //second things.........
 
   
   return (
@@ -15,11 +15,12 @@ const Comp1 = () => {
     <h1>Hello from comp1</h1>
     <p>This is a sample component.</p>
     <button className={`btn ${theme}`} onClick={
-      toggleTheme
+      toggleThemeeeee
     } >switch to  {theme==="light"?"dark":"light  "} mode  </button> 
     </div >
   );
 };
+
 
  
 
